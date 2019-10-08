6144489c0347ee5a82b390bb7531d1bc47417851
granit <graniitkastrati@gmail.com>
2019-10-08 17:34:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ function updateObjectWithKeyAndValue(object,key,value){
         object['key']='value'
         return object
 }
-var updateObjectWithKeyAndValue(object,key,value){
-        object['key']='value'
-        return object
-}
